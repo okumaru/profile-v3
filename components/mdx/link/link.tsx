71e16533be3f00001699a0ec51e615f0e92eb0b8
Andhika M. Wijaya <okumaru07@gmail.com>
2023-03-16 18:58:34 +0700

    add url to project

--- components/mdx/link/link.tsx
+++ components/mdx/link/link.tsx
@@ -16,7 +16,9 @@ export const LinkHeader = ({ href, slug, publishedOn, children }: Prop) => (
       <span className="text-gray-700 group-hover:text-gray-400 text-lg no-underline">#</span>
       <span className="text-xl font-semibold underline">{children}</span>
     </a>
-    <p className="text-base p-summary text-neutral-400">{slug}</p>
+    
+    {slug && <p className="text-base p-summary text-neutral-400">{slug}</p>}
+    
     {publishedOn && <div className="flex gap-2 items-center mt-2">
       <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
