1477568ea99ff81683c2266d4e7557fd0c236e42
Andhika M. Wijaya <okumaru07@gmail.com>
2023-03-21 22:08:45 +0700

    add dark & light mode

--- components/layouts/post-layout.tsx
+++ components/layouts/post-layout.tsx
@@ -22,9 +22,9 @@ export default function PostLayout({meta, children }: Props) {
     <Header />
     <div>
       <div className="mb-8">
-        <span className="inline-block bg-zinc-700 py-1 px-2 rounded">{meta.publishedOn}</span>
+        <span className="inline-block bg-zinc-300 dark:bg-zinc-700 py-1 px-2 rounded">{meta.publishedOn}</span>
         <h1 className="text-2xl font-semibold underline underline-offset-2">{meta.title}</h1>
-        { meta.slug && <p className="text-base p-summary text-neutral-400">{meta.slug}</p> }
+        { meta.slug && <p className="text-base p-summary text-neutral-500">{meta.slug}</p> }
       </div>
       <div>
         {children}
