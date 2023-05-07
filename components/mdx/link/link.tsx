09fb09cce7fd6c35f74ccfd57300e0c0183fb29f
Andhika M. Wijaya <okumaru07@gmail.com>
2023-05-07 15:57:28 +0700

    Fix error react html attribute (#4)
    * fix error react html attribute
    
    ---------
    
    Co-authored-by: Andhika <andhika.marta@arkamaya.co.id>

--- components/mdx/link/link.tsx
+++ components/mdx/link/link.tsx
@@ -30,8 +30,8 @@ export const LinkHeader = ({ href, slug, publishedOn, children }: Prop) => (
     {slug && <p className="text-base p-summary text-neutral-500">{slug}</p>}
     
     {publishedOn && <div className="flex gap-2 items-center mt-2">
-      <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+      <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       {publishedOn}
     </div>}
