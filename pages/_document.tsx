cd62f3a0e640e2c3f3dcc4687d7393f8f75d1788
Andhika M. Wijaya <okumaru07@gmail.com>
2023-05-07 16:00:55 +0700

    add ga (#5)
    Co-authored-by: Andhika <andhika.marta@arkamaya.co.id>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_TAG_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){
+              window.dataLayer.push(arguments)
+            }
+
+            gtag('js', new Date());
+            gtag('config', '${process.env.G_TAG_ID}');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
