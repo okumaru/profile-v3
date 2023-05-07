2957608030e7f0eb18852e83ce6d0715cbb7ce5a
Andhika M. Wijaya <okumaru07@gmail.com>
2023-05-07 20:59:16 +0700

    add info use cookies (#6)
    * add info use cookies
    
    ---------
    
    Co-authored-by: Andhika <andhika.marta@arkamaya.co.id>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
+import CookieAlert from "../components/cookies-alert"
 
 export default function Document() {
   return (
@@ -20,10 +21,31 @@ export default function Document() {
             gtag('config', '${process.env.G_TAG_ID}');
           `}
         </Script>
+        <Script id="cookies-alert" strategy="afterInteractive">
+          {`
+
+            const cookiesAlertBtn = document.getElementById('cookies-alert-btn');
+            const cookiesAlertSection = document.getElementById("cookies-alert");
+
+            const hideCookiesAlert = () => {
+              cookiesAlertSection.style.display = 'none';
+            }
+
+            setTimeout(() => {
+              hideCookiesAlert();
+            }, 15000);
+
+            cookiesAlertBtn.addEventListener('click', () => {
+              hideCookiesAlert();
+            });
+
+          `}
+        </Script>
       </Head>
-      <body>
+      <body className='relative'>
         <Main />
         <NextScript />
+        <CookieAlert />
       </body>
     </Html>
   )
